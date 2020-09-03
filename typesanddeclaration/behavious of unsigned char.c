d6cdfffbaeddc026e7c9f0cc4cab05cb8b233da2
Shubhashish Saha <68582303+Shubhashish778@users.noreply.github.com>
2020-09-03 10:46:43 +0530

    Create behavious of unsigned char.c
    It gives out infinite loop due to char being of 256 bytes.

---
+++ typesanddeclaration/behavious of unsigned char.c
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for (unsigned char c=0; c<=255;++c)
+    {
+        cout<<c<<endl;
+    }
+}
+
+
+OUTPUT:
+It gives out ascii character of range 0-255.
